8ea0c5ed5fb1b1a574c8d4d7151becded929a564
dalyd <david.daly@mongodb.com>
2015-04-09 15:33:15 -0400

    Added variants of filtered tests with a non-selective filter (nothing filtered out)

--- testcases/filtered_index.js
+++ testcases/filtered_index.js
@@ -16,6 +16,13 @@ var setupTestFiltered = function (collection) {
     collection.ensureIndex( { x : 1 }, { filter : { a : { $lt : 500 } } } );
     collection.getDB().getLastError();
 }
+
+var setupTestFilteredNonSelective = function (collection) {
+    setupTest(collection);
+    collection.ensureIndex( { x : 1 }, { filter : { a : { $lt : 4800 } } } );
+    collection.getDB().getLastError();
+}
+
 var setupTestIndexed = function (collection) {
     setupTest(collection);
     collection.ensureIndex( { x : 1 });
@@ -77,6 +84,67 @@ tests.push( { name : "Filtered_Index.v1.filter-mixuse.lte",
               tags: ['monthly'],
               pre: function( collection ) {
                   setupTestFiltered(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : {$lte : { "#RAND_INT" : [ 0 , 4800 ] } } } }
+              ] } );
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-used",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : {"#RAND_INT" : [ 0, 500 ]}, a : {$lt : 500  } } }
+              ] } );
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-unused",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : {"#RAND_INT" : [ 500, 4800 ]}, a : {$gte : 500  } } }
+              ] } );
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-mixuse",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : { "#RAND_INT" : [ 0 , 4800 ] } } }
+              ] } );
+
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-used.lte",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : {$lte : {"#RAND_INT" : [ 0, 500 ]}}, a : {$lt : 500  } } }
+              ] } );
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-unused.lte",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "find", query:  { x : {$lte : {"#RAND_INT" : [ 500, 4800 ]}}, a : {$gte : 500  } } }
+              ] } );
+
+tests.push( { name : "Filtered_Index.Non_Selective.v1.filter-mixuse.lte",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
               },
 
               ops : [
--- testcases/filtered_index_insert.js
+++ testcases/filtered_index_insert.js
@@ -11,6 +11,13 @@ var setupTestFiltered = function (collection) {
     collection.ensureIndex( { x : 1 }, { filter : { a : { $lt : 500 } } } );
     collection.getDB().getLastError();
 }
+
+var setupTestFilteredNonSelective = function (collection) {
+    setupTest(collection);
+    collection.ensureIndex( { x : 1 }, { filter : { a : { $lt : 4800 } } } );
+    collection.getDB().getLastError();
+}
+
 var setupTestIndexed = function (collection) {
     setupTest(collection);
     collection.ensureIndex( { x : 1 });
@@ -41,6 +48,37 @@ tests.push( { name : "Filtered_Index_Insert.v1.filter-mixuse",
               tags: ['monthly'],
               pre: function( collection ) {
                   setupTestFiltered(collection);
+              },
+
+              ops : [
+                  { op: "insert", doc:  { x : { "#RAND_INT" : [ 0 , 4800 ] }, a : { "#RAND_INT" : [ 0 , 4800 ] } } }
+              ] } );
+
+
+tests.push( { name : "Filtered_Index_Insert.Non_Selective.v1.filter-used",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "insert", doc:  { x : {"#RAND_INT" : [ 0, 500 ]}, a : {"#RAND_INT" : [ 0, 500 ]} } }
+              ] } );
+
+tests.push( { name : "Filtered_Index_Insert.Non_Selective.v1.filter-unused",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
+              },
+
+              ops : [
+                  { op: "insert", doc:  { x : {"#RAND_INT" : [ 500, 4800 ]}, a : {"#RAND_INT" : [ 500, 4800 ]} } }
+              ] } );
+
+tests.push( { name : "Filtered_Index_Insert.Non_Selective.v1.filter-mixuse",
+              tags: ['monthly'],
+              pre: function( collection ) {
+                  setupTestFilteredNonSelective(collection);
               },
 
               ops : [

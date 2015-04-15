b3b7a80d5e7dce03542582ff6bd1df304d7068be
dalyd <david.daly@mongodb.com>
2015-04-15 11:11:24 -0400

    New tests for baselining noise in mongo-perf

---
+++ testcases/baseline_noise.js
@@ -0,0 +1,88 @@
+if ( typeof(tests) != "object" ) {
+    tests = [];
+}
+
+// This contacts the server and generates an error. Should make a log messagexo
+tests.push( { name: "Commands.illegalOp",
+              tags: ['skip'],
+              ops: [
+                  { op: "command", ns : "#B_DB", command : { "notExist" : 1 } }
+              ] } );
+
+// This does not contact the server. Noop in benchrun loop
+tests.push( { name: "Commands.nop",
+              tags: ['skip','baseline'],
+              ops: [
+                  { op: "nop" }
+              ] } );
+
+// This does not contact the server. Sleeps for 1 ms 
+tests.push( { name: "Commands.sleep.1ms.local",
+              tags: ['skip','baseline'],
+              ops: [
+                  { op: "sleepMicros", micros : NumberLong(1000) }
+              ] } );
+
+// This does not contact the server. Sleeps for 10 ms 
+tests.push( { name: "Commands.sleep.10ms.local",
+              tags: ['skip','baseline'],
+              ops: [
+                  { op: "sleepMicros", micros : NumberLong(10000) }
+              ] } );
+
+// This does not contact server. Copies a value in benchrun loop
+tests.push( { name: "Commands.let",
+              tags: ['skip','baseline'],
+              ops: [
+                  { op: "let", target: "x", value : 1 }
+              ] } );
+
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.noop",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "#B_DB",
+                    command : { noop : 1}}
+              ] } );
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.ping",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "#B_DB",
+                    command : { ping : 1}}
+              ] } );
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.uri",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "#B_DB",
+                    command : { whatsmyuri : 1}}
+              ] } );
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.buildinfo",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "#B_DB",
+                    command : { buildinfo : 1}}
+              ] } );
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.isMaster",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "#B_DB",
+                    command : { ismaster : 1}}
+              ] } );
+// This contacts the server and does a noop on the server
+tests.push( { name: "Commands.sleep",
+              tags: ['skip', 'baseline'],
+              ops: [
+                  { op: "command", 
+                    ns : "admin",
+                    command : { sleep : 1, w:false, secs:0.01}}
+              ] } );
+
